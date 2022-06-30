0a2a9eeac95fa7dadac544368d2d60ce8497c844
Rvn0xsy <rvn0xsy@gmail.com>
2022-06-30 17:01:17 +0800

    Update

---
+++ josCreateProcess/josCreateProcess.cpp
@@ -0,0 +1,194 @@
+﻿#define _WIN32_DCOM
+
+#include <iostream>
+using namespace std;
+#include <comdef.h>
+#include <Wbemidl.h>
+
+#pragma comment(lib, "wbemuuid.lib")
+
+int main(int iArgCnt, char** argv)
+{
+    HRESULT hres;
+
+    // Step 1: --------------------------------------------------
+    // Initialize COM. ------------------------------------------
+
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres))
+    {
+        cout << "Failed to initialize COM library. Error code = 0x"
+            << hex << hres << endl;
+        return 1;                  // Program has failed.
+    }
+
+    // Step 2: --------------------------------------------------
+    // Set general COM security levels --------------------------
+
+    hres = CoInitializeSecurity(
+        NULL,
+        -1,                          // COM negotiates service
+        NULL,                        // Authentication services
+        NULL,                        // Reserved
+        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation
+        NULL,                        // Authentication info
+        EOAC_NONE,                   // Additional capabilities 
+        NULL                         // Reserved
+    );
+
+
+    if (FAILED(hres))
+    {
+        cout << "Failed to initialize security. Error code = 0x"
+            << hex << hres << endl;
+        CoUninitialize();
+        return 1;                      // Program has failed.
+    }
+
+    // Step 3: ---------------------------------------------------
+    // Obtain the initial locator to WMI -------------------------
+
+    IWbemLocator* pLoc = NULL;
+
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator, (LPVOID*)&pLoc);
+
+    if (FAILED(hres))
+    {
+        cout << "Failed to create IWbemLocator object. "
+            << "Err code = 0x"
+            << hex << hres << endl;
+        CoUninitialize();
+        return 1;                 // Program has failed.
+    }
+
+    // Step 4: ---------------------------------------------------
+    // Connect to WMI through the IWbemLocator::ConnectServer method
+
+    IWbemServices* pSvc = NULL;
+
+    // Connect to the local root\cimv2 namespace
+    // and obtain pointer pSvc to make IWbemServices calls.
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"),
+        NULL,
+        NULL,
+        0,
+        NULL,
+        0,
+        0,
+        &pSvc
+    );
+
+    if (FAILED(hres))
+    {
+        cout << "Could not connect. Error code = 0x"
+            << hex << hres << endl;
+        pLoc->Release();
+        CoUninitialize();
+        return 1;                // Program has failed.
+    }
+
+    cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
+
+
+    // Step 5: --------------------------------------------------
+    // Set security levels for the proxy ------------------------
+
+    hres = CoSetProxyBlanket(
+        pSvc,                        // Indicates the proxy to set
+        RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx 
+        RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx 
+        NULL,                        // Server principal name 
+        RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+        NULL,                        // client identity
+        EOAC_NONE                    // proxy capabilities 
+    );
+
+    if (FAILED(hres))
+    {
+        cout << "Could not set proxy blanket. Error code = 0x"
+            << hex << hres << endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;               // Program has failed.
+    }
+
+    // Step 6: --------------------------------------------------
+    // Use the IWbemServices pointer to make requests of WMI ----
+
+    // set up to call the Win32_Process::Create method
+    BSTR MethodName = SysAllocString(L"Create");
+    BSTR ClassName = SysAllocString(L"Win32_Process");
+
+    IWbemClassObject* pClass = NULL;
+    hres = pSvc->GetObject(ClassName, 0, NULL, &pClass, NULL);
+
+    IWbemClassObject* pInParamsDefinition = NULL;
+    hres = pClass->GetMethod(MethodName, 0,
+        &pInParamsDefinition, NULL);
+
+    IWbemClassObject* pClassInstance = NULL;
+    hres = pInParamsDefinition->SpawnInstance(0, &pClassInstance);
+
+    // Create the values for the in parameters
+    VARIANT varCommand;
+    varCommand.vt = VT_BSTR;
+    varCommand.bstrVal = _bstr_t(L"notepad.exe");
+
+    // Store the value for the in parameters
+    hres = pClassInstance->Put(L"CommandLine", 0,
+        &varCommand, 0);
+    wprintf(L"The command is: %s\n", V_BSTR(&varCommand));
+
+    // Execute Method
+    IWbemClassObject* pOutParams = NULL;
+    hres = pSvc->ExecMethod(ClassName, MethodName, 0,
+        NULL, pClassInstance, &pOutParams, NULL);
+
+    if (FAILED(hres))
+    {
+        cout << "Could not execute method. Error code = 0x"
+            << hex << hres << endl;
+        VariantClear(&varCommand);
+        SysFreeString(ClassName);
+        SysFreeString(MethodName);
+        pClass->Release();
+        pClassInstance->Release();
+        pInParamsDefinition->Release();
+        pOutParams->Release();
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;               // Program has failed.
+    }
+
+    // To see what the method returned,
+    // use the following code.  The return value will
+    // be in &varReturnValue
+    VARIANT varReturnValue;
+    hres = pOutParams->Get(_bstr_t(L"ReturnValue"), 0,
+        &varReturnValue, NULL, 0);
+
+
+    // Clean up
+    //--------------------------
+    VariantClear(&varCommand);
+    VariantClear(&varReturnValue);
+    SysFreeString(ClassName);
+    SysFreeString(MethodName);
+    pClass->Release();
+    pClassInstance->Release();
+    pInParamsDefinition->Release();
+    pOutParams->Release();
+    pLoc->Release();
+    pSvc->Release();
+    CoUninitialize();
+    return 0;
+}
---
+++ josOpenRDP-Execute/josOpenRDP-Execute.cpp
@@ -0,0 +1,218 @@
+﻿// josOpenRDP-Execute.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+
+#define _WIN32_DCOM
+
+#include <iostream>
+
+#include <comdef.h>
+#include <Wbemidl.h>
+
+#pragma comment(lib, "wbemuuid.lib")
+using namespace std;
+
+INT InvokeWMIProcess()
+{
+    HRESULT hres;
+
+    // Step 1: --------------------------------------------------
+    // Initialize COM. ------------------------------------------
+
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres))
+    {
+        cout << "Failed to initialize COM library. Error code = 0x"
+            << hex << hres << endl;
+        return 1;                  // Program has failed.
+    }
+
+    // Step 2: --------------------------------------------------
+    // Set general COM security levels --------------------------
+
+    hres = CoInitializeSecurity(
+        NULL,
+        -1,                          // COM negotiates service
+        NULL,                        // Authentication services
+        NULL,                        // Reserved
+        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation
+        NULL,                        // Authentication info
+        EOAC_NONE,                   // Additional capabilities 
+        NULL                         // Reserved
+    );
+
+
+    if (FAILED(hres))
+    {
+        cout << "Failed to initialize security. Error code = 0x"
+            << hex << hres << endl;
+        CoUninitialize();
+        return 1;                      // Program has failed.
+    }
+
+    // Step 3: ---------------------------------------------------
+    // Obtain the initial locator to WMI -------------------------
+
+    IWbemLocator* pLoc = NULL;
+
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator, (LPVOID*)&pLoc);
+
+    if (FAILED(hres))
+    {
+        cout << "Failed to create IWbemLocator object. "
+            << "Err code = 0x"
+            << hex << hres << endl;
+        CoUninitialize();
+        return 1;                 // Program has failed.
+    }
+
+    // Step 4: ---------------------------------------------------
+    // Connect to WMI through the IWbemLocator::ConnectServer method
+
+    IWbemServices* pSvc = NULL;
+
+    // Connect to the local root\cimv2 namespace
+    // and obtain pointer pSvc to make IWbemServices calls.
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2\\TerminalServices"),
+        NULL,
+        NULL,
+        0,
+        NULL,
+        0,
+        0,
+        &pSvc
+    );
+
+    if (FAILED(hres))
+    {
+        cout << "Could not connect. Error code = 0x"
+            << hex << hres << endl;
+        pLoc->Release();
+        CoUninitialize();
+        return 1;                // Program has failed.
+    }
+
+    cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
+
+
+    // Step 5: --------------------------------------------------
+    // Set security levels for the proxy ------------------------
+
+    hres = CoSetProxyBlanket(
+        pSvc,                        // Indicates the proxy to set
+        RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx 
+        RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx 
+        NULL,                        // Server principal name 
+        RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+        NULL,                        // client identity
+        EOAC_NONE                    // proxy capabilities 
+    );
+
+    if (FAILED(hres))
+    {
+        cout << "Could not set proxy blanket. Error code = 0x"
+            << hex << hres << endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;               // Program has failed.
+    }
+
+    // Step 6: --------------------------------------------------
+    // Use the IWbemServices pointer to make requests of WMI ----
+
+    // set up to call the Win32_Process::Create method
+    BSTR MethodName = SysAllocString(L"SetAllowTSConnections");
+    // \\DESKTOP-JLLN6HH\ROOT\CIMV2\TerminalServices:Win32_TerminalServiceSetting
+    BSTR ClassName = SysAllocString(L"Win32_TerminalServiceSetting");
+
+    IWbemClassObject* pClass = NULL;
+    hres = pSvc->GetObject(ClassName, 0, NULL, &pClass, NULL);
+
+    IWbemClassObject* pInParamsDefinition = NULL;
+    hres = pClass->GetMethod(MethodName, 0,
+        &pInParamsDefinition, NULL);
+
+    IWbemClassObject* pClassInstance = NULL;
+    hres = pInParamsDefinition->SpawnInstance(0, &pClassInstance);
+
+    // Create the values for the in parameters
+    VARIANT AllowTSConnections;
+    short Open = TRUE;
+    AllowTSConnections.vt = VT_BOOL;
+    AllowTSConnections.pboolVal = &Open;
+
+    // Store the value for the in parameters
+    hres = pClassInstance->Put(L"AllowTSConnections", 0,
+        &AllowTSConnections, 0);
+
+
+    // Execute Method
+    IWbemClassObject* pOutParams = NULL;
+    hres = pSvc->ExecMethod(ClassName, MethodName, 0,
+        NULL, pClassInstance, &pOutParams, NULL);
+
+    if (FAILED(hres))
+    {
+        cout << "Could not execute method. Error code = 0x"
+            << hex << hres << endl;
+        VariantClear(&AllowTSConnections);
+        SysFreeString(ClassName);
+        SysFreeString(MethodName);
+        pClass->Release();
+        pClassInstance->Release();
+        pInParamsDefinition->Release();
+        pOutParams->Release();
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;               // Program has failed.
+    }
+
+    // To see what the method returned,
+    // use the following code.  The return value will
+    // be in &varReturnValue
+    VARIANT varReturnValue;
+    hres = pOutParams->Get(_bstr_t(L"ReturnValue"), 0,
+        &varReturnValue, NULL, 0);
+
+
+    // Clean up
+    //--------------------------
+    VariantClear(&AllowTSConnections);
+    VariantClear(&varReturnValue);
+    SysFreeString(ClassName);
+    SysFreeString(MethodName);
+    pClass->Release();
+    pClassInstance->Release();
+    pInParamsDefinition->Release();
+    pOutParams->Release();
+    pLoc->Release();
+    pSvc->Release();
+    CoUninitialize();
+    return 0;
+}
+
+
+int main()
+{
+    InvokeWMIProcess();
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
